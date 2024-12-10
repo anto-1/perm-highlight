26627ab68d248f753c42491e40534f6ceec547bb
GitHub Action <action@github.com>
2024-12-10 05:52:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/anto1/permhighlight/MyBundle.kt
+++ src/main/kotlin/com/github/anto1/permhighlight/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.anto1.permhighlight
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/anto1/permhighlight/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/anto1/permhighlight/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.anto1.permhighlight.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
